e0ddf442e90cb4fb565fe17e81e306c235e02b1a
Michael W. Clark <mclark@denimsocial.com>
2021-12-21 01:02:42 -0600

    Initial commit. API and service

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # trellis-law-interview
+
+python3 -m venv env
+source env/bin/activate
+
+python -m pip install -r requirements.txt
---
+++ trellis/trellisNumbersAPI/views.py
@@ -0,0 +1,27 @@
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from .services import num_to_english
+import json
+
+
+
+@csrf_exempt
+def get_num_to_english(request):
+    try:
+        number = None
+        if request.method == 'GET':
+            number = request.GET.get('number')
+        elif request.method == 'POST':
+            number = json.loads(request.body).get('number')
+
+        if len(str(number)) > 102:
+            raise ValueError('Number is too large')
+
+        num_in_english = num_to_english(number)
+        return JsonResponse({'status': 'ok', 'num_in_english': num_in_english})
+        
+    except (ValueError, TypeError) as e:
+        return JsonResponse({'status': 'error', 'message': 'Invalid number'}, status=500)
+        
+
